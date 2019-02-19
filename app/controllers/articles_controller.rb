b4ab60146d4249862e9dfd3e4080802578a79f40
MohamedRaza Pirbhai <razaP_13@live.com>
2019-02-19 17:32:09 +0000

    work done, added a few css lines. played around with the faker also

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,40 @@
+class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+
+    def show
+      @article = Article.find(params[:id])
+  end
+
+  def new
+    @articles = Article.new
+  end
+
+  def create
+    Article.create(article_params)
+    redirect_to articles_path
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @articles = Article.find(params[:id])
+    @articles.update(article_params)
+    redirect_to articles_path(@articles)
+  end
+
+  def destroy
+    @articles = Article.find(params[:id])
+    @articles.destroy
+    redirect_to articles_path
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :details)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+ get "articles", to: "articles#index"
+ get 'articles/new', to: 'articles#new'
+ get 'articles/:id', to: 'articles#show', as: "article"
+ post 'articles', to: 'articles#create'
+ get 'articles/:id/edit', to: 'articles#edit', as: "edit"
+ patch 'articles/:id', to: 'articles#update', as: "update"
+ delete 'articles/:id', to: 'articles#destroy'
 end
